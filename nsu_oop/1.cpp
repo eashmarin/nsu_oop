f99869d235bcea79b18e7a320092617ebe27b952
Евгений <ashmarin.evgeny@gmail.com>
2021-11-03 18:36:14 +0700

    added func. shrink, getTrit, DEBUG & overload operator ==

--- nsu_oop/1.cpp
+++ nsu_oop/1.cpp
@@ -2,12 +2,7 @@
 #include <vector>
 #define uint unsigned int
 #define uint_index(index) (index * 2 / 8 / sizeof(uint))
-//#define True 00
-//#define Unknown 10
-//#define False 01
-//#define trit_size 2
 using namespace std;
-// 00 - True, 01 - False, 10 - Unknown
 // 11 - Unknown, 01 - False, 10 - True
 enum Trit{
 	False = 1, 
@@ -15,7 +10,7 @@ enum Trit{
 	Unknown = 3
 };
 
-string int2Trit(uint value) {
+string trit2str(uint value) {
 	switch (value) {
 	case 1:
 		return "False";
@@ -28,34 +23,41 @@ string int2Trit(uint value) {
 
 class TritSet {
 private:
-
 	int size;
-	int index;
 	vector<uint> data;
-	int size_remained;
 public:
+	void DEBUG() {
+		for (int i = 0; i < data.size(); i++) {
+			cout << "data[" << i << "] = " << data[i] << endl;
+		}
+	}
 	TritSet() {
 		size = 0;
 	}
 	TritSet(const int size) {
 		resize(size);
 	}
-	void resize(uint size) {
-		this->size = size;
-		size_remained = (round)(size * 2 / (sizeof(uint) * 8)) + 1;
-		//size_remained += ((size / 2) % (size_remained)) / 2;
-		data.resize(size_remained);
+	void resize(uint lastIndex) {
+		uint uintSize = (round)(lastIndex * 2 / (sizeof(uint) * 8)) + 1;					// Сделать параметром не посл индекс, а размер (tritSize) 
+		data.resize(uintSize);
+		this->size = lastIndex;
 	}
 	const int capacity() const {
 		return data.size();
 	}
 	uint getValue(uint index) const {
-		if (capacity() <= index)
-			return 3;
+		if (capacity() <= uint_index(index))
+			return Unknown;
 		return data[uint_index(index)];
 	}
 	void setValue(uint value, uint index) {
 		data[uint_index(index)] = value;
+	}
+	void shrink() {
+		int i = size - 1;
+		while (i > 0 && getValue(i) == 0)
+			i--;
+		resize(i);
 	}
 	class ProxyTrit {
 	public:
@@ -63,24 +65,45 @@ public:
 			this->set = set;
 			this->index = index;
 			if (uint_index(index) == 0)
-				offset = index;
+				offset = index * 2;
 			else
 				offset = (index % uint_index(index)) * 2 - 2;
+		}
+		Trit getTrit() {
+			uint mask = 3 << offset;
+			uint result = (set->getValue(index) & mask) >> offset;
+			switch (result) {
+			case 1:
+				return Trit::False;
+			case 2:
+				return Trit::True;
+			default:
+				return Trit::Unknown;
+			}
 		}
 		void operator=(Trit value) {
 			if (value == Unknown)
 				return;
-			if (set->capacity() <= index * 2 / 8 / sizeof(uint)) {
+			if (set->capacity() <= uint_index(index)) {
 				set->resize(index);
 			}
 			uint mask = value << offset;
 			set->setValue(set->getValue(index) | mask, index);
 		}
-		friend ostream& operator<< (std::ostream &out, TritSet::ProxyTrit trit) {
-			uint mask = 3 << trit.offset;
-			return out << int2Trit((trit.set->getValue(trit.index) & mask) >> (trit.offset));
+		bool operator== (Trit value) {
+			if (set->capacity() <= uint_index(index)) {
+				if (value == Unknown)
+					return true;
+				else
+					return false;
+			}
+			return value == getTrit();
 		}
-
+		friend ostream& operator<< (std::ostream &out, TritSet::ProxyTrit trit) {
+			
+			return out << trit2str(trit.getTrit());
+		}
+		 
 	private:
 		TritSet* set;
 		uint index;
@@ -93,16 +116,18 @@ public:
 
 };
 
-
-int main() {
+int main(int argc, char* argv[]) {
 	TritSet set(500);
-	set[499] = Trit::False;
-	set[500] = Trit::True;
-	set[498] = Trit::True;
-	set[1000] = Trit::Unknown;
-	cout << set[1000] << set.capacity() << endl;
-	//cout << set[19] << " - " << &set[19] << endl;
-	//cout << sizeof(uint);
-	//cout << set.capacity() << endl;
+	cout << "capacity before = " << set.capacity() << endl;
+	set[0] = Trit::False;
+	set[1] = Trit::True;
+	set[3] = Trit::True;
+	set[447] = Trit::True;
+	set.shrink();
+	cout << "capacity after shrink = " << set.capacity() << endl;
+	if (set[55500] == Unknown)
+		cout << "TRUE!!!" << endl;
+	cout << "set[447] = " << set[447] << endl;
+
 	return 0;
 }
